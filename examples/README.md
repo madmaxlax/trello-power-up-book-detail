bae94fd183436a66b9157a0b6f65d2fb5c1156f4
Max Struever <maxstruever@gmail.com>
2026-02-03 12:51:59 -0500

    refactor: enhance book data parsing and improve error handling in search functionality

---
+++ examples/README.md
@@ -0,0 +1,33 @@
+# Trello Power-Up Examples
+
+This directory contains example files from the original Glitch Trello Power-Up template. These demonstrate various Power-Up capabilities but are not used in the Book Detail Power-Up.
+
+## Example Files
+
+### JavaScript Examples
+- **orig-client.js** - Original complete template client
+- **section.js** - Card back section example (Yellowstone attachment)
+- **modal.js** - Modal resize demonstration
+- **board-bar.js** - Board bar navigation example
+- **settings.js** - Power-Up settings page example
+- **celebrate.js** - Confetti animation demo
+
+### HTML Pages
+- **section.html** - Card back section page
+- **modal.html** - Modal demonstration page
+- **board-bar.html** - Board bar page
+- **settings.html** - Settings page
+- **celebrate.html** - Celebration/confetti page
+- **authorize.html** - Authorization flow example
+- **api-client-authorize.html** - API client authorization
+- **auth-success.html** - Auth callback page
+
+### CSS
+- **card-back-section.css** - Debug styles with dashed outlines
+- **board-bar.css** - Board bar styling
+- **modal.css** - Modal styling
+
+## Reference
+
+For complete Trello Power-Up documentation, see:
+https://developer.atlassian.com/cloud/trello/guides/power-ups/your-first-power-up/
--- js/client.js
+++ js/client.js
@@ -76,232 +76,22 @@ t.getAll();
 
 */
 
-var GLITCH_ICON = 'https://cdn.glitch.com/2442c68d-7b6d-4b69-9d13-feab530aa88e%2Fglitch-icon.svg?1489773457908';
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-// 
 var GRAY_ICON = 'https://cdn.glitch.global/2757198c-0a15-451b-9f67-1ae65b9ba93b/book-open-thin-svgrepo-com.svg?v=1738893407137';
-// var GRAY_ICON = 'https://cdn.glitch.me/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
-// var randomBadgeColor = function () {
-//   return ['green', 'yellow', 'red', 'none'][Math.floor(Math.random() * 4)];
-// };
-
-// var getBadges = function (t) {
-//   return t.card('name')
-//     .get('name')
-//     .then(function (cardName) {
-//       console.log('We just loaded the card name for fun: ' + cardName);
-
-//       return [{
-//         // dynamic badges can have their function rerun after a set number
-//         // of seconds defined by refresh. Minimum of 10 seconds.
-//         dynamic: function () {
-//           // we could also return a Promise that resolves to this as well if we needed to do something async first
-//           return {
-//             title: 'Detail Badge', // for detail badges only
-//             text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-//             icon: GRAY_ICON, // for card front badges only
-//             color: randomBadgeColor(),
-//             refresh: 10 // in seconds
-//           };
-//         }
-//       }, {
-//         // its best to use static badges unless you need your badges to refresh
-//         // you can mix and match between static and dynamic
-//         title: 'Detail Badge', // for detail badges only
-//         text: 'Static',
-//         icon: GRAY_ICON, // for card front badges only
-//         color: null
-//       }, {
-//         // card detail badges (those that appear on the back of cards)
-//         // also support callback functions so that you can open for example
-//         // open a popup on click
-//         title: 'Popup Detail Badge', // for detail badges only
-//         text: 'Popup',
-//         icon: GRAY_ICON, // for card front badges only
-//         callback: function (context) { // function to run on click
-//           return context.popup({
-//             title: 'Card Detail Badge Popup',
-//             url: './settings.html',
-//             height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
-//           });
-//         }
-//       }, {
-//         // or for simpler use cases you can also provide a url
-//         // when the user clicks on the card detail badge they will
-//         // go to a new tab at that url
-//         title: 'URL Detail Badge', // for detail badges only
-//         text: 'URL',
-//         icon: GRAY_ICON, // for card front badges only
-//         url: 'https://trello.com/home',
-//         target: 'Trello Landing Page' // optional target for above url
-//       }];
-//     });
-// };
-
-// var boardButtonCallback = function (t) {
-//   return t.popup({
-//     title: 'Popup List Example',
-//     items: [
-//       {
-//         text: 'Open Modal',
-//         callback: function (t) {
-//           return t.modal({
-//             url: './modal.html', // The URL to load for the iframe
-//             args: { text: 'Hello', fullscreen: true }, // Optional args to access later with t.arg('text') on './modal.html'
-//             accentColor: '#F2D600', // Optional color for the modal header 
-//             height: 500, // Initial height for iframe; not used if fullscreen is true
-//             fullscreen: true, // Whether the modal should stretch to take up the whole screen
-//             callback: () => console.log('Goodbye.'), // optional function called if user closes modal (via `X` or escape)
-//             title: 'Hello, Modal!', // Optional title for modal header
-//             // You can add up to 3 action buttons on the modal header - max 1 on the right side.
-//             actions: [{
-//               icon: GRAY_ICON,
-//               url: 'https://google.com', // Opens the URL passed to it.
-//               alt: 'Leftmost',
-//               position: 'left',
-//             }, {
-//               icon: GRAY_ICON,
-//               callback: (tr) => tr.popup({ // Callback to be called when user clicks the action button.
-//                 title: 'Settings',
-//                 url: 'settings.html',
-//                 height: 164,
-//               }),
-//               alt: 'Second from left',
-//               position: 'left',
-//             }, {
-//               icon: GRAY_ICON,
-//               callback: () => console.log('🏎'),
-//               alt: 'Right side',
-//               position: 'right',
-//             }],
-//           })
-//         }
-//       },
-//       {
-//         text: 'Open Board Bar',
-//         callback: function (t) {
-//           return t.boardBar({
-//             url: './board-bar.html',
-//             height: 200
-//           })
-//             .then(function () {
-//               return t.closePopup();
-//             });
-//         }
-//       }
-//     ]
-//   });
-// };
-
-// var restApiCardButtonCallback = function (t) {
-//   return t.getRestApi()
-//     .isAuthorized()
-//     .then(function (authorized) {
-//       if (!authorized) {
-//         // You might be tempted to call client.authorize from a capability handler like the one we are in right now.
-//         // Unfortunately this does not register as a click by the browser, and it will block the popup. Instead, we need to
-//         // open a t.popup from our capability handler, and load an iframe that contains a button that calls client.authorize.
-//         return t.popup({
-//           title: 'Authorize Trello\'s REST API',
-//           url: './api-client-authorize.html',
-//         })
-//       } else {
-//         return t.popup({
-//           title: "Make a choice",
-//           items: [{
-//             // We'll use the client on the authorization page to make an example request.
-//             text: 'Make an example request',
-//             callback: function (t) {
-//               return t.popup({
-//                 title: 'Authorize Trello\'s REST API',
-//                 url: './api-client-authorize.html',
-//               })
-//             }
-//           }, {
-//             // You can de-authorize the REST API client with a call to .clearToken()
-//             text: 'Unauthorize',
-//             callback: function (t) {
-//               return t.getRestApi()
-//                 .clearToken()
-//                 .then(function () {
-//                   t.alert('You\'ve successfully deauthorized!');
-//                   t.closePopup();
-//                 })
-//             }
-//           }]
-//         })
-//       }
-//     });
-// }
-
-// var cardButtonCallback = function(t){
-//   // Trello Power-Up Popups are actually pretty powerful
-//   // Searching is a pretty common use case, so why reinvent the wheel
-//   var items = ['acad', 'arch', 'badl', 'crla', 'grca', 'yell', 'yose'].map(function(parkCode){
-//     var urlForCode = 'http://www.nps.gov/' + parkCode + '/';
-//     var nameForCode = '🏞 ' + parkCode.toUpperCase();
-//     return {
-//       text: nameForCode,
-//       url: urlForCode,
-//       callback: function(t){
-//         // In this case we want to attach that park to the card as an attachment
-//         // but first let's ensure that the user can write on this model
-//         if (t.memberCanWriteToModel('card')){
-//           return t.attach({ url: urlForCode, name: nameForCode })
-//           .then(function(){
-//             // once that has completed we should tidy up and close the popup
-//             return t.closePopup();
-//           });
-//         } else {
-//           console.log("Oh no! You don't have permission to add attachments to this card.")
-//           return t.closePopup(); // We're just going to close the popup for now.
-//         };
-//       }
-//     };
-//   });
-
-//   // we could provide a standard iframe popup, but in this case we
-//   // will let Trello do the heavy lifting
-//   return t.popup({
-//     title: 'Popup Search Example',
-//     items: items, // Trello will search client-side based on the text property of the items
-//     search: {
-//       count: 5, // How many items to display at a time
-//       placeholder: 'Search National Parks',
-//       empty: 'No parks found'
-//     }
-//   });
-
-// in the above case we let Trello do the searching client side
-// but what if we don't have all the information up front?
-// no worries, instead of giving Trello an array of `items` you can give it a function instead
-/*
-return t.popup({
-  title: 'Popup Async Search',
-  items: function(t, options) {
-    // use options.search which is the search text entered so far
-    // and return a Promise that resolves to an array of items
-    // similar to the items you provided in the client side version above
-  },
-  search: {
-    placeholder: 'Start typing your search',
-    empty: 'Huh, nothing there',
-    searching: 'Scouring the internet...'
+// Helper function to safely parse book data
+var parseBookData = function(data) {
+  if (!data) return null;
+  try {
+    return typeof data === 'string' ? JSON.parse(data) : data;
+  } catch (e) {
+    console.error('Failed to parse bookData:', e, data);
+    return null;
   }
-});
-*/
-// };
+};
 
 var getBadges = function (t) {
   return t.get('card', 'shared', 'bookData').then(function (bookData) {
-    if (bookData) {
-      try {
-        bookData = JSON.parse(bookData);
-      } catch (e) {
-        console.error('Failed to parse bookData:', e, bookData);
-      }
-    }
+    bookData = parseBookData(bookData);
 
     if (!bookData) {
       return [];
@@ -325,11 +115,7 @@ TrelloPowerUp.initialize({
 
   'card-back-section': function (t, options) {
     return t.get('card', 'shared', 'bookData').then(function (bookData) {
-      try {
-        bookData = JSON.parse(bookData);
-      } catch (e) {
-        console.error('Failed to parse bookData:', e, bookData);
-      }
+      bookData = parseBookData(bookData);
       let sectionTitle = '';
 
       if (!bookData) {
@@ -350,23 +136,12 @@ TrelloPowerUp.initialize({
   },
   'card-buttons': function (t, options) {
     return t.get('card', 'shared', 'bookData').then(function (bookData) {
-      try {
-        bookData = JSON.parse(bookData);
-      } catch (e) {
-        // console.error('Failed to parse bookData:', e, bookData);
-      }
-      let sectionTitle = '';
-
-      if (!bookData) {
-        sectionTitle = 'Search for Book';
-      }
-      else sectionTitle = 'Change Book';
+      bookData = parseBookData(bookData);
 
       return [
-
         {
           icon: GRAY_ICON,
-          text: 'Search for Book',
+          text: bookData ? 'Change Book' : 'Search for Book',
           callback: (t) => t.popup({
             title: 'Search for Book',
             url: './search-books.html',
